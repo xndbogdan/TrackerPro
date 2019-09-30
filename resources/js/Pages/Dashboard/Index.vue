39239ed6b3b51928e0dac61b8c8d7cfceae38ac5
Bogdan Mihai <bogdanmihai@Bogdans-MacBook-Pro.local>
2019-09-30 14:31:58 +0300

    Fixed css issue

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -112,13 +112,3 @@
     },
   }
 </script>
-<style>
-    .fade-enter-active, .fade-leave-active {
-        transition: opacity .5s, background-color .5s;
-    }
-
-    .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */
-    {
-        opacity: 0;
-    }
-</style>
